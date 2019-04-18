07fb7ed9f26cd363df060a14ee82ca6d0f6dfef4
Prabhjot SANDHU <prabhjot.sandhu@mail.mcgill.ca>
2019-04-18 10:14:13 -0400

    added PAPI with DEBUG

--- c/main.c
+++ c/main.c
@@ -13,6 +13,11 @@
 #include "utils.h"
 #include "config.h"
 
+#ifdef DEBUG
+#include "papi.h"
+#define NUM_EVENTS 5
+#endif
+
 #define OUTER_MAX 30
 
 int main(int argc, char* argv[])
@@ -27,6 +32,10 @@ int main(int argc, char* argv[])
   double exec_arr[OUTER_MAX];
   double sum = 0, mean = 0, sd, variance = 0;
   int inner, inner_max;
+  #ifdef DEBUG
+  int events[NUM_EVENTS] = {PAPI_L1_DCM, PAPI_L2_DCM, PAPI_L2_DCH, PAPI_L3_TCM, PAPI_TLB_DM}, ret, event_set=PAPI_NULL;
+  long long values[NUM_EVENTS];
+  #endif
 
   if(argc < 2){
     fprintf(stderr, "Usage: %s [martix-market-filename]\n", argv[0]);
@@ -36,10 +45,33 @@ int main(int argc, char* argv[])
     if((f = fopen(argv[1], "r")) == NULL)
       exit(1);
   }
+ 
+  #ifdef DEBUG
+  ret = PAPI_library_init(PAPI_VER_CURRENT);
+  if (ret != PAPI_VER_CURRENT && ret > 0) {
+    fprintf(stderr,"PAPI library version mismatch!\n");
+    exit(1);
+  }
+  if (ret < 0) {
+    fprintf(stderr, "Initialization error!\n");
+    exit(1);
+  }
+  ret = PAPI_create_eventset(&event_set);
+  if(ret != PAPI_OK){
+    fprintf(stderr, "create event set error\n");
+    exit(1);
+  }
+  ret = PAPI_add_events(event_set, events, NUM_EVENTS);
+  if(ret != PAPI_OK){
+    fprintf(stderr, "add event error\n");
+    fprintf(stderr, "%d %s\n", ret, PAPI_strerror(ret));
+    exit(1);
+  }
+  #endif
 
   anz = count_nnz(f);
-
   rewind(f);
+
   if (mm_read_banner(f, &matcode) != 0)
   {
     fprintf(stderr, "Could not process Matrix Market banner.\n");
@@ -94,10 +126,6 @@ int main(int argc, char* argv[])
     fprintf(stderr, "couldn't allocate y using calloc");
     exit(1);
   }
-
-  /* NOTE: when reading in doubles, ANSI C requires the use of the "l"  */
-  /*   specifier as in "%lg", "%lf", "%le", otherwise errors will occur */
-  /*  (ANSI C X3.159-1989, Sec. 4.9.6.2, p. 136 lines 13-15)            */
 
   k = 0;
   if(mm_is_symmetric(matcode)){
@@ -162,7 +190,6 @@ int main(int argc, char* argv[])
 
   quickSort(row, col, coo_val, 0, anz-1);
 
-
   if(anz > 1000000) inner_max = 1;
   else if (anz > 100000) inner_max = 10;
   else if (anz > 50000) inner_max = 50;
@@ -171,6 +198,12 @@ int main(int argc, char* argv[])
   else if(anz > 100) inner_max = 10000;
 
   if(!string_compare(argv[2], "coo")){
+    #ifdef DEBUG
+    if ((ret = PAPI_start(event_set)) != PAPI_OK) {
+      fprintf(stderr, "PAPI failed to start counters: %s\n", PAPI_strerror(ret));
+      exit(1);
+    }
+    #endif
     for (i=0; i<OUTER_MAX; i++){
       zero_arr(N, y);
       start = clock();
@@ -180,6 +213,12 @@ int main(int argc, char* argv[])
       exec_arr[i] = 1.0/1000000 * 2 * anz * inner_max/ ((double)(stop - start)/ CLOCKS_PER_SEC); 
       sum += (double)(stop - start);
     }
+    #ifdef DEBUG
+    if ((ret = PAPI_read(event_set, values)) != PAPI_OK) {
+      fprintf(stderr, "PAPI failed to read counters: %s\n", PAPI_strerror(ret));
+      exit(1);
+    }
+    #endif
     printf("%d,", N);
     printf("%d,", anz);
     mean  = 1.0/1000000 * 2 * anz * inner_max * OUTER_MAX / ((double)(sum) / CLOCKS_PER_SEC);
@@ -191,10 +230,24 @@ int main(int argc, char* argv[])
     printf("%f,", sd);
     printf("%g,", mean);
     printf("%d", fletcher_sum(y, M)); 
+    #ifdef DEBUG
+    printf("\n");
+    printf("L1 data cache misses is %lld\n", values[0]);
+    printf("L2 data cache misses is %lld\n", values[1]);
+    printf("L2 data cache hits is %lld\n", values[2]);
+    printf("L3 data cache misses is %lld\n", values[3]);
+    printf("Data TLB misses is %lld\n", values[4]);
+    #endif
   }
 
   if(!string_compare(argv[2], "csr")){
     coo_csr(anz, N, row, col, coo_val, row_ptr, colind, val);
+    #ifdef DEBUG
+    if ((ret = PAPI_start(event_set)) != PAPI_OK) {
+      fprintf(stderr, "PAPI failed to start counters: %s\n", PAPI_strerror(ret));
+      exit(1);
+    }
+    #endif
     for (i=0; i<OUTER_MAX; i++){
       zero_arr(N, y);
       start = clock();
@@ -204,6 +257,12 @@ int main(int argc, char* argv[])
       exec_arr[i] = 1.0/1000000 * 2 * anz * inner_max / ((double)(stop - start)/ CLOCKS_PER_SEC); 
       sum += (double)(stop - start);
     }
+    #ifdef DEBUG
+    if ((ret = PAPI_read(event_set, values)) != PAPI_OK) {
+      fprintf(stderr, "PAPI failed to read counters: %s\n", PAPI_strerror(ret));
+      exit(1);
+    }
+    #endif
     mean  = 1.0/1000000 * 2 * anz * inner_max * OUTER_MAX / ((double)(sum) / CLOCKS_PER_SEC);
     for (i=0; i<OUTER_MAX; i++){
       variance += (mean - exec_arr[i]) * (mean - exec_arr[i]);
@@ -213,10 +272,25 @@ int main(int argc, char* argv[])
     printf("%f,", sd);
     printf("%g,", mean);
     printf("%d", fletcher_sum(y, M)); 
+    #ifdef DEBUG
+    printf("\n");
+    printf("L1 data cache misses is %lld\n", values[0]);
+    printf("L2 data cache misses is %lld\n", values[1]);
+    printf("L2 data cache hits is %lld\n", values[2]);
+    printf("L3 data cache misses is %lld\n", values[3]);
+    printf("Data TLB misses is %lld\n", values[4]);
+    #endif
   }
+
   if(!string_compare(argv[2], "dia")){
     coo_csr(anz, N, row, col, coo_val, row_ptr, colind, val);
     csr_dia(row_ptr, colind, val, &offset, &dia_data, N, &num_diags, &stride);
+    #ifdef DEBUG
+    if ((ret = PAPI_start(event_set)) != PAPI_OK) {
+      fprintf(stderr, "PAPI failed to start counters: %s\n", PAPI_strerror(ret));
+      exit(1);
+    }
+    #endif
     for (i=0; i<OUTER_MAX; i++){
       zero_arr(N, y);
       start = clock();
@@ -226,6 +300,12 @@ int main(int argc, char* argv[])
       exec_arr[i] = 1.0/1000000 * 2 * anz * inner_max / ((double)(stop - start)/ CLOCKS_PER_SEC); 
       sum += (double)(stop - start);
     }
+    #ifdef DEBUG
+    if ((ret = PAPI_read(event_set, values)) != PAPI_OK) {
+      fprintf(stderr, "PAPI failed to read counters: %s\n", PAPI_strerror(ret));
+      exit(1);
+    }
+    #endif
     mean  = 1.0/1000000 * 2 * anz * inner_max * OUTER_MAX / ((double)(sum) / CLOCKS_PER_SEC);
     for (i=0; i<OUTER_MAX; i++){
       variance += (mean - exec_arr[i]) * (mean - exec_arr[i]);
@@ -235,12 +315,27 @@ int main(int argc, char* argv[])
     printf("%f,", sd);
     printf("%g,", mean);
     printf("%d", fletcher_sum(y, M)); 
+    #ifdef DEBUG
+    printf("\n");
+    printf("L1 data cache misses is %lld\n", values[0]);
+    printf("L2 data cache misses is %lld\n", values[1]);
+    printf("L2 data cache hits is %lld\n", values[2]);
+    printf("L3 data cache misses is %lld\n", values[3]);
+    printf("Data TLB misses is %lld\n", values[4]);
+    #endif
     free(dia_data);
     free(offset);
   }
+
   if(!string_compare(argv[2], "ell")){
     coo_csr(anz, N, row, col, coo_val, row_ptr, colind, val);
     csr_ell(row_ptr, colind, val, &indices, &ell_data, N, &num_cols);
+    #ifdef DEBUG
+    if ((ret = PAPI_start(event_set)) != PAPI_OK) {
+      fprintf(stderr, "PAPI failed to start counters: %s\n", PAPI_strerror(ret));
+      exit(1);
+    }
+    #endif
     for (i=0; i<OUTER_MAX; i++){
       zero_arr(N, y);
       start = clock();
@@ -250,6 +345,12 @@ int main(int argc, char* argv[])
       exec_arr[i] = 1.0/1000000 * 2 * anz * inner_max / ((double)(stop - start)/ CLOCKS_PER_SEC); 
       sum += (double)(stop - start);
     }
+    #ifdef DEBUG
+    if ((ret = PAPI_read(event_set, values)) != PAPI_OK) {
+      fprintf(stderr, "PAPI failed to read counters: %s\n", PAPI_strerror(ret));
+      exit(1);
+    }
+    #endif
     mean  = 1.0/1000000 * 2 * anz * inner_max * OUTER_MAX / ((double)(sum) / CLOCKS_PER_SEC);
     for (i=0; i<OUTER_MAX; i++){
       variance += (mean - exec_arr[i]) * (mean - exec_arr[i]);
@@ -259,6 +360,14 @@ int main(int argc, char* argv[])
     printf("%f,", sd);
     printf("%g,", mean);
     printf("%d", fletcher_sum(y, M)); 
+    #ifdef DEBUG
+    printf("\n");
+    printf("L1 data cache misses is %lld\n", values[0]);
+    printf("L2 data cache misses is %lld\n", values[1]);
+    printf("L2 data cache hits is %lld\n", values[2]);
+    printf("L3 data cache misses is %lld\n", values[3]);
+    printf("Data TLB misses is %lld\n", values[4]);
+    #endif
     free(ell_data);
     free(indices);
   }
